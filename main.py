4e3acbe36e26ee7d4c79a72c9579abd4d4d07464
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-10 23:07:40 +0500

    Show video

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+smile_detector = cv.CascadeClassifier("haarcascade_smile.xml")
+
+# Read video (change filename to 0 if need webcam)
+video = cv.VideoCapture("videos/vid_test_smile.3gp")
+
+while True:
+    # Read currunt frame
+    successful, frame = video.read()
+
+    # If can't read currunt frame come out of loop
+    if not successful:
+        break
+
+    cv.imshow("Smile Detector", frame)
+
+    key = cv.waitKey(1)
+
+    if key==27:
+        break # If "Esc" is pressed, come out of loop
+
+video.release()
+cv.destroyAllWindows()
